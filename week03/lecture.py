8bbc60166d3117c4f911f3f9750f4f0670060c0c
Gabriela Domiciano Avellar <gabriela.d@hotmail.com>
2024-02-13 21:48:14 +0000

    varible types

---
+++ week03/lecture.py
@@ -0,0 +1,21 @@
+# watching the class and doing the program with the professor
+# Gabriela Domiciano 
+# variables types
+# f ferramenta que temos para formatação de textos em Python.
+# As f-strings vão servir para que você consiga colocar uma variável dentro de um texto,
+# e isso é feito utilizando a letra “f” antes do texto e colocando a sua variável dentro de {} chaves.
+
+
+answer_of_question = "blue"
+
+
+print (f"ansewr is {answer_of_question}")
+print ("answer is " + answer_of_question)
+
+answer = 11
+print ("answer is " + str(answer))
+
+# to print the type of varible
+print (f"type of the answer is {type(answer_of_question)}")
+
+

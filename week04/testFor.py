facd139d2eac32f893c7042ffacd6b66ee0b1734
Gabriela Domiciano Avellar <gabriela.d@hotmail.com>
2024-02-22 17:36:47 +0000

    lecture04

---
+++ week04/testFor.py
@@ -0,0 +1,35 @@
+#loops
+#GAbriela Domiciano
+#for elements in lists
+#do something
+#for numbers in range(1,10)
+#do somenthing
+
+
+
+boats = ['sigma', 'x yacht', 'Swan']
+
+for boat in boats:
+    print("i'd rather be out in a ", boat)
+
+
+
+
+boats = ['sigma', 'x yacht', 'Swan']
+
+for boat in boats:
+    print("i'd rather be out in a ", boat, sep="", end="\n\n" )
+
+    
+    
+    
+    #count 1 to 10
+
+    for i in range(1,10):
+        print (i)
+print("all done i is now", i)
+
+
+
+if "Swan" in boats:
+    print ("that is a boat")
---
+++ week04/testWhile.py
@@ -0,0 +1,38 @@
+# Gabriela Domiciano
+
+# all loops
+# For loops - with list
+
+
+#count = 0
+#while (count < 10):
+ #   print("count is ", count)  
+
+ #infinit lup
+    
+
+# count 0 to 9
+count = 0
+while (count < 10):
+    print("count is ", count)  
+    count = count + 1
+print ("at the end count is ", count)
+
+
+
+
+count = 10
+while count >=0:
+    print("countdown ", count)  
+    count  -= 1
+print ("blast off")
+       
+
+
+# sentinal controled loop =  when you do something until some event happens
+val = input("type csomething (q to quit):")
+while val != 'q':
+    print ("Hi mon")
+    val = input("q to quit:")
+
+    print(" all done")

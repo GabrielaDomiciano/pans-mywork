51a1688592b53c3b6fd74eae4cd5a86c3c57431b
Gabriela Domiciano Avellar <gabriela.d@hotmail.com>
2024-03-14 08:54:09 -0300

    list

---
+++ week05/lists.py
@@ -0,0 +1,23 @@
+# Listas
+# Gabriela Domiciano
+'''
+boats = ['Sigma', 23, [1,2,3], {}]
+
+# boats = boats + [1,2,3]
+for boat in boats:
+    print(type(boat))
+
+print(boats)
+'''
+
+ages =[12,21,23,34]
+
+sum = 0
+for age in ages:
+    sum += age
+print(sum)
+
+
+string = "1234567890"
+print(string[6:])
+print(string[-6:])
---
+++ week05/tuples.py
@@ -0,0 +1,5 @@
+#Tuples
+t = (1,2,3)
+x, y, z = t
+print (x)
+print (y)

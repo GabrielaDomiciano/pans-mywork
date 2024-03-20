5a9308668b81ee293f84ad373f7995eb5cda82ce
Gabriela Domiciano Avellar <gabriela.d@hotmail.com>
2024-03-20 20:05:17 +0000

    labo5

---
+++ week05/lab_datastructures.py
@@ -0,0 +1,59 @@
+numberOfQuestions = 5
+averageAge = 23.4
+debugMode = True
+name = "joe"
+ages = []
+months = ('Jan', 'Feb', 'Mar')
+book = {}
+stuff = [ 12 , 'Fred', False, {}]
+someone = dict(firstname = "joe")
+me = {
+"firstName" : "Andrew",
+"teaching" : [{
+"courseName" : "programming",
+"semester" : 1
+},{
+"courseName" : "Data Representation",
+"semester" : 2
+}
+]
+}
+
+Questions
+What are the variable types of the following variables in the code above
+a. numberOfQuestions   -integers
+b. averageAge  - Floating
+c. debugMode  boolen 
+d. name  -String
+e. ages   list
+f. months  tuple
+g. months[1]  str
+h. book  dict 
+i. stuff list 
+j. stuff[2] boolen false 
+k. someone  dict  
+l. someone["firstname"] str  
+m. me  dict 
+n. me["teaching"]    list (is is nested in the dict)
+o. me["teaching"][0]["semester"]   int
+p is a trick question look at it carefully
+p. me["teaching"][0]["coursename"]
+
+
+
+a. int
+b. float
+c. boolean
+d. str
+e. list
+f. tuple
+g. str
+h. dict
+i. list
+j. boolean (False)
+k. dict
+l. str
+m. dict
+n. list (is is nested in the dict)
+o. int
+p. undefined (the code has a capital N in courseName)

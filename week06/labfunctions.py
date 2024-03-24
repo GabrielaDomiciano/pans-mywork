29642938348e0867c04fa1cdd945e9bd37fb5b4e
Gabriela Domiciano Avellar <gabriela.d@hotmail.com>
2024-03-24 16:53:12 +0000

    week6

---
+++ week06/labfunctions.py
@@ -0,0 +1,15 @@
+# function yo takes one parameter
+def yo(a):
+return a * 2
+# here we are calling the function yo
+# and are passing in an argument, 3
+print(yo(3)
+
+
+
+# a. Nothing! Nothing will appear when we run this, there is no print
+#  function
+# b. print(yo(3)
+# or
+# var = yo(3)
+# print(var)

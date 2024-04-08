d8bcc5a1d19840a3b63c8913f747eaf7dc58d3de
Gabriela Domiciano Avellar <gabriela.d@hotmail.com>
2024-04-08 21:46:03 +0100

    week09

---
+++ week09/except.py
@@ -0,0 +1,16 @@
+
+#filename = 'data.txt'
+import sys
+
+#print (sys.argv)
+try:
+    filename = sys.argv[1]
+    print(filename)
+    with open(filename) as fp:
+        print(fp.read())
+        
+except IndexError as ie: 
+    print("please enter the filename as an argument")
+    #print(ie)
+except FileNotFoundError:
+    print(f"file {filename} not found, please enter a filename that exists")
